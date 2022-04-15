e8ad221e5709ee93b5d1789033e985d1ba567001
Gustavo <gugu.r.azevedo@gmail.com>
2022-04-15 20:50:39 -0300

    Configure CORS

--- src/server.js
+++ src/server.js
@@ -19,7 +19,12 @@ const PORT = process.env.PORT || 3000;
 if (process.env.NODE_ENV !== 'production') app.use(morgan('dev'));
 app.use('/static', express.static(path.join(__dirname, 'public')));
 app.use(express.json());
-// app.use(cors());
+app.use(
+    cors({
+        origin: '*',
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    })
+);
 app.use(helmet());
 app.use('/v1', routes);
 
